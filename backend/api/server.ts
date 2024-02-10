f41cd5810af821df336d6cc850cde8e75228bd44
Dasajton <david.sajitz.94@gmail.com>
2024-02-10 22:19:38 +0100

    setup mock CRUD routes for flashcards (and tested with Postman)

--- backend/api/server.ts
+++ backend/api/server.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { flashcardRouter } from "./routers/flashcardRouter";
 
 export const app = express();
 
@@ -7,3 +8,5 @@ app.get("/", (_req, res) => {
     appName: "API for LearnApp version 0.1",
   });
 });
+
+app.use("/api/flashcards", flashcardRouter);
